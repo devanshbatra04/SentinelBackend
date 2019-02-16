fdb88b1df49bcedab62284f273ebf552ece4354f
Devansh Batra <devanshbatra04@gmail.com>
2019-02-16 23:50:15 +0530

    virus total requests limit

--- virustotal.py
+++ virustotal.py
@@ -9,29 +9,41 @@ def quickScan(file):
               'resource': hash_file(file)}
     headers = {"Accept-Encoding": "gzip, deflate",
                "User-Agent": "gzip,  My Python requests library example client or username"}
-    response = requests.get('https://www.virustotal.com/vtapi/v2/file/report', params=params, headers=headers)
-    json_response = response.json()
-    # print(json_response)
-    return {
-        'total scans': json_response.get('total'),
-        'positives': json_response.get('positives'),
-        'scan date': json_response.get('scan_date'),
-        'message': json_response.get('verbose_msg'),
-        'file': file
-    }
+    try:
+        response = requests.get('https://www.virustotal.com/vtapi/v2/file/report', params=params, headers=headers)
+        json_response = response.json()
+
+        return {
+            'total scans': json_response.get('total'),
+            'positives': json_response.get('positives'),
+            'scan date': json_response.get('scan_date'),
+            'message': json_response.get('verbose_msg'),
+            'file': file
+        }
+    except:
+        return {
+            'message': 'Too many VirusTotal requests, try again later'
+        }
 
 
 def lookup_process(id):
     file_list = psutil.Process(int(id)).open_files()
     open_files = map(lambda x: x.path, file_list)
-    return list(map(lambda file: quickScan(file), open_files))
+    return list(map(lambda file: quickScan(file), list(open_files)))
 
 
 def adv_scan(filePath):
     params = {'apikey': 'd21b1c0487ea217eda6e715bd9a6663c05c7a1655a3167767c0d85528a402344'}
     files = {'file': (filePath.split('/')[-1], open(filePath, 'rb'))}
     response = requests.post('https://www.virustotal.com/vtapi/v2/file/scan', files=files, params=params)
-    json_response = response.json()
-    if json_response["verbose_msg"] == "Scan request successfully queued, come back later for the report":
-        addScheduledFile(filePath, json_response["sha1"])
-    return json_response["verbose_msg"]
+    try:
+        json_response = response.json()
+        if json_response["verbose_msg"] == "Scan request successfully queued, come back later for the report":
+            addScheduledFile(filePath, json_response["sha1"])
+        return {
+            'message': json_response["verbose_msg"]
+        }
+    except:
+        return {
+            'message': 'Too many VirusTotal requests, try again later'
+        }
