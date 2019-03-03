6abcb63bee6e20a1a957e1dd3f5fc1d85e20e9f3
Yash Agarwal <yash23.agarwal@gmail.com>
2019-03-03 07:39:23 +0530

    seems complete for windows

--- models.py
+++ models.py
@@ -26,13 +26,19 @@ def addToBlacklist(ip, port):
     try:
         db.session.add(user)
         db.session.commit()
-        if os.name == 'nt':
-            os.system("netsh advfirewall firewall add rule name='IP block' dir=in protocol=tcp remoteip={} localport={} action=block".format(ip, port))
-            return "blocked"
         if port != '*':
             command = ("iptables -A INPUT -p tcp --sport {} -s {} -j DROP").format(str(port), str(ip))
+            if os.name == 'nt':
+                command = "netsh advfirewall firewall add rule name=IPblock dir=in protocol=tcp remoteip={} localport={} action=block".format(ip, port)
+                print(command)
             os.system(command)
+            return "blocked"
         else:
+            if os.name == 'nt':
+                command = "netsh advfirewall firewall add rule name=IPblock dir=in protocol=tcp remoteip={} action=block".format(ip)
+                print(command)
+                os.system(command)
+                return "blocked"
             rule = iptc.Rule()
             rule.protocol = 0
             rule.src = str(ip)
@@ -50,7 +56,10 @@ def removeFromBlacklist(ip, port):
         user = Blacklist.query.filter_by(ip=ip).filter_by(port=port)
         user.delete()
         db.session.commit()
-        os.system("netsh advfirewall firewall delete rule name='IP block' dir=in protocol=tcp remoteip={} localport={}".format(ip, port))
+        command = "netsh advfirewall firewall delete rule name=IPblock dir=in protocol=tcp remoteip={} localport={}".format(ip, port)
+        if port=='*':
+            command = "netsh advfirewall firewall delete rule name=IPblock dir=in protocol=tcp remoteip={}".format(ip)
+        os.system(command)
         return "unblocked"
     if port != '*':
         user = Blacklist.query.filter_by(ip=ip).filter_by(port=port)
